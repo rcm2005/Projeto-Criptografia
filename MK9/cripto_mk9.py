9c8df1631d79359249dd35b1b11f818a7ced09f7
rafael <rcm2005@icloud.com>
2023-08-24 16:01:10 -0300

    quase acabando a versão mk9

--- MK9/cripto_mk9.py
+++ MK9/cripto_mk9.py
@@ -196,10 +196,30 @@ def codificar():
 'I', '7', 'i', 'J', '8', 'j', 'K', '9', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 'Ç', 'ç', ',', '.', '*', 'Á', 'á', 'É', 'é', 'Í', 'í', 'Ó', 'ó', 'Ú', 'ú', 'Ã', 'ã', 'Õ', 'õ', 'Â', 'â', 'Ê', 'ê', 'Î', 'î', 'Ô', 'ô', 'Û', 'û'], ['à', 'A', 'a', 'B', '0', 'b', 'C', '1', 'c', 'D', '2', 'd', 'E', '3', 'e', 'F', '4', 'f', 'G', '5', 'g', 'H', '6', 'h', 'I', '7', 'i', 'J', '8', 'j', 'K', '9', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 'Ç', 'ç', ',', '.', '*', 'Á', 'á', 'É', 'é', 'Í', 'í', 'Ó', 'ó', 'Ú', 'ú', 'Ã', 'ã', 'Õ', 'õ', 'Â', 'â', 'Ê', 'ê', 'Î', 
 'î', 'Ô', 'ô', 'Û', 'û', 'À']]
 
-    # for i in range(palavracrypto):
-    #      for o in range(alfabetomensagem):
-    #           if palavracrypto[i] == alfabetomensagem[o]:
-        
+    chavesub = []
+    posicaosub = []
+
+    mensagemfinal = []
+
+    cont = 0
+    som = 0
+    for i in range(len(chaveiro)):
+        cont +=1
+        soma += chaveiro[i]
+        if cont == 11:
+            chavesub.append(som)
+            cont = 0
+            som = 0 
+
+    for i in range(len(palavracrypto)):
+         for m in range(alfabetomensagem):
+              if palavracrypto[i] == alfabetomensagem[m]:
+                posicaosub.append(alfabetomensagem[m])               
+
+
+    # for s in range(len(posicaosub)):
+
+            
 
     return frasecrypto
 
